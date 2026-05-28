3cb9aa67b16a3ef84f39979392b545eb9e121b8b
Juarren Helgerson <juarren2002@gmail.com>
2026-05-27 22:08:36 -0500

    added to this

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,26 +1,3 @@
-let myScript = document.createElement('li');
-let textScript = document.createTextNode('Hotdogs');
-myScript.appendChild(textScript);
-let scriptPosition = document.getElementsByTagName('ul')[0];
-scriptPosition.appendChild(myScript);
-
-
-let newScript = document.createElement('ol');
-document.body.appendChild(newScript);
-let newScriptChild = document.createElement('li');
-let newText = document.createTextNode('pizza');
-newScript.appendChild(newScriptChild);
-newScriptChild.appendChild(newText);
-console.log(newScript);
-let scriptPosition2 = document.getElementsByTagName('ol')[0];
-
-
-let removeScript = document.getElementsByTagName('li')[2];
-let removalContainer = removeScript.parentNode;
-console.log(removalContainer);
-
-
-
-
-
-
+let myScript = document.getElementById("box1");
+myScript.className = 'BigBox';
+console.log(myScript.className);
