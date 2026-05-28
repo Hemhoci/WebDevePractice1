126fbb25f0b3b761dffe22d00df2c5b98d7dd7a7
Juarren Helgerson <juarren2002@gmail.com>
2026-05-27 23:51:06 -0500

    added to repository

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,3 +1,13 @@
 let myScript = document.getElementById("box1");
 myScript.className = 'BigBox';
 console.log(myScript.className);
+
+let myScript2 = document.getElementsByTagName('li').item(2);
+myScript2.setAttribute('class','Power');
+console.log(myScript2);
+let myScriptModified = document.querySelectorAll('li');
+console.log(myScriptModified);
+myScript2.removeAttribute('class');
+console.log(myScript2);
+myScript2.removeAttribute('id');
+console.log(myScript2);
